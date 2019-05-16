3ed5ea75744c359cf70c8a3bea499d5e98f4c28c
Rike <hello@ueberdiespree.de>
2019-05-16 19:46:49 +0200

    added two auth strategies for different routes

---
+++ api/plugins/auth.plugin.js
@@ -0,0 +1,49 @@
+// const jwtSecret = "NeverShareYourSecret";
+// const strategy = "jwt";
+
+// module.exports = {
+//   plugin: {
+//     name: "auth",
+//     register
+//   },
+//   strategy
+// };
+
+// async function register(server, options) {
+//   await server.register(require("hapi-auth-jwt2"));
+
+//   const validate = (decodedToken, request, h) => {
+//     let { user } = decodedToken;
+//     if (!user) {
+//       return { isValid: false };
+//     }
+//     /* check for additional auth requirements if necessary */
+//     return {
+//       isValid: true,
+//       credentials: { user }
+//     };
+//   };
+
+//   server.auth.strategy(strategy, strategy, {
+//     key: jwtSecret,
+//     validate,
+//     verifyOptions: { algorithms: ["HS256"] }
+//   });
+
+//   // server.auth.default(strategy);
+
+//   server.method("createToken", createToken, {});
+// }
+
+// function createToken(user) {
+//   const Jwt = require("jsonwebtoken");
+
+//   const { email, _id } = user;
+
+//   token = Jwt.sign({ user: { email, _id } }, jwtSecret, {
+//     algorithm: "HS256",
+//     expiresIn: "1m"
+//   });
+
+//   return token;
+// }
