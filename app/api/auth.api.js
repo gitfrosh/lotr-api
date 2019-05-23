8d8d75b430992dd2cf3bdef823092f296495223a
Rike <hello@ueberdiespree.de>
2019-05-23 23:18:58 +0200

    isLogged in logic

--- app/api/auth.api.js
+++ app/api/auth.api.js
@@ -6,7 +6,7 @@ var cookie_options = {
   ttl: 365 * 24 * 60 * 60 * 1000, // expires a year from today
   encoding: "none", // we already used JWT to encode
   isSecure: false, // warm & fuzzy feelings
-  isHttpOnly: true, // prevent client alteration
+  isHttpOnly: false, // prevent client alteration
   clearInvalid: false, // remove invalid cookies
   strictHeader: true, // don't allow violations of RFC 6265
   path: "/" // set the cookie for all routes
