7e351582ca208501da45f1dbffc508687942a85c
Rike <hello@ueberdiespree.de>
2019-06-15 07:20:39 +0200

    typos

--- views/home.jsx
+++ views/home.jsx
@@ -88,11 +88,14 @@ class View extends React.Component {
                 <div className="panel dark">
                   <div className="panel-head">What is this?</div>
                   <div className="panel-body">
-                    <em>Mellon</em>, this is the API (<a href="/documentation#1">What the hell is an
-                    Application Programming Interface?</a>) that serves your needs
-                    regarding data about <strong>The Lord of the Rings</strong>,
-                    the epic books by J. R. R. Tolkien and the official movie
-                    adaptions by Peter Jackson.
+                    <em>Mellon</em>, this is the API (
+                    <a href="/documentation#1">
+                      What the hell is an Application Programming Interface?
+                    </a>
+                    ) that serves your needs regarding data about{" "}
+                    <strong>The Lord of the Rings</strong>, the epic books by J.
+                    R. R. Tolkien and the official movie adaptions by Peter
+                    Jackson.
                   </div>
                 </div>
               </div>
@@ -104,7 +107,9 @@ class View extends React.Component {
               <div className="box col-2" />
               <div className="box col-8">
                 <div className="panel dark">
-                  <div className="panel-head">Try it now!</div>
+                  <div className="panel-head">
+                    Try it now! (maybe "/book" ?)
+                  </div>
                   <div className="panel-body">
                     <div>
                       <label htmlFor="try-api">Get</label>
@@ -129,8 +134,8 @@ class View extends React.Component {
                     </div>
                     <div className="notice dark">
                       There are many more endpoints available, but you need to{" "}
-                      <a href="/sign-up">sign up</a> to get the access key to
-                      load all the data first. Get a glimpse into the{" "}
+                      <a href="/sign-up">sign up</a> to obtain an access key.
+                      Get a glimpse into the{" "}
                       <a href="/documentation">documentation</a> to check out
                       all accessable datasets.
                     </div>
