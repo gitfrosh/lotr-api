224597685dc2ba7f652ac1bb1429ffd90e0a4f78
Ulrike Exner <ulrike.exner@iav.de>
2020-06-22 10:05:05 +0200

    .gitignore node_modules

--- v2/backend/node_modules/node-addon-api/index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  include: `"${__dirname}"`,
-  gyp: path.join(__dirname, 'node_api.gyp:nothing'),
-  isNodeApiBuiltin: true,
-  needsFlag: false
-};
