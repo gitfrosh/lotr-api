65bd069243c27a59e3fa5ee6f8caa1369d8cfade
rike <hello@rike.dev>
2020-08-02 19:33:23 +0200

    more styling, fix user account

--- v2/frontend/src/pages/Account.jsx
+++ v2/frontend/src/pages/Account.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from "react";
 import {getUserInfo} from "../helpers/api";
+import { useHistory } from "react-router-dom";
 
 function Account() {
-  const [userInfo, setUserInfo] = useState({});
+  const [userInfo, setUserInfo] = useState(null);
   const [loading, setLoading] = useState(true);
+  const history = useHistory();
 
   useEffect(() => {
-    getUserInfo().then(user => setUserInfo(user)) 
-    setLoading(false);
+    getUserInfo().then(user => {
+      if (user) setUserInfo(user)
+      if (!user) history.push('/login')
+    } ) 
+    setLoading(false); 
+
   }, []);
-
   if (loading) return <span>Loading..</span>;
-
   return (
     <div>
       <h2>Account</h2>
@@ -19,13 +23,13 @@ function Account() {
       <div class="card">
         <div class="section dark">
           <p>
-            <b>Registered email adress</b>: {userInfo.email}
+            <b>Registered email adress</b>: {userInfo && userInfo.email}
           </p>
         </div>
 
         <div class="section dark">
           <p>
-            <b>Access token:</b> {userInfo.access_token}
+            <b>Access token:</b> {userInfo && userInfo.access_token}
             <em> (Include this in your API calls!)</em>
           </p>
         </div>
