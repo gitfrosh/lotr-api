16a271cafefadd71fa5f931ddb978800c5a649ea
rike <hello@rike.dev>
2020-10-26 21:54:34 +0100

    add missing anchor in documentation; restructure examples

--- frontend/src/pages/Documentation.jsx
+++ frontend/src/pages/Documentation.jsx
@@ -24,6 +24,9 @@ function Documentation() {
           <li>
             <a href="#4">Which routes are available?</a>
           </li>
+          <li>
+            <a href="#5">May I use pagination, sorting and filtering?</a>
+          </li>
         </ul>
       </p>
       <br />
@@ -96,9 +99,8 @@ function Documentation() {
         <div className="section">
           <h2>Which routes are available?</h2>
           <p>
-            All routes must be prefixed with{" "}
-            <b>https://the-one-api.dev/v2</b>. Only the{" "}
-            /book endpoint is available without authentication.
+            All routes must be prefixed with <b>https://the-one-api.dev/v2</b>.
+            Only the /book endpoint is available without authentication.
           </p>
 
           <table>
@@ -114,8 +116,10 @@ function Documentation() {
                 <td data-label="Endpoint">
                   <strong>/book</strong>
                 </td>
-                <td  data-label="Response">List of all "The Lord of the Rings" books</td>
-                <td  data-label="Token required">no</td>
+                <td data-label="Response">
+                  List of all "The Lord of the Rings" books
+                </td>
+                <td data-label="Token required">no</td>
               </tr>
               <tr>
                 <td data-label="Endpoint">/book/&#123;id&#125;</td>
@@ -124,7 +128,9 @@ function Documentation() {
               </tr>
               <tr>
                 <td data-label="Endpoint">/book/&#123;id&#125;/chapter</td>
-                <td data-label="Response">Request all chapters of one specific book</td>
+                <td data-label="Response">
+                  Request all chapters of one specific book
+                </td>
                 <td data-label="Token required"></td>
               </tr>
               <tr>
@@ -167,7 +173,9 @@ function Documentation() {
               </tr>
               <tr>
                 <td data-label="Endpoint">/character/&#123;id&#125;/quote</td>
-                <td data-label="Response">Request all movie quotes of one specific character</td>
+                <td data-label="Response">
+                  Request all movie quotes of one specific character
+                </td>
                 <td data-label="Token required">yes</td>
               </tr>
               <tr>
@@ -198,12 +206,12 @@ function Documentation() {
           </table>
         </div>
       </div>
-      <div className="card fluid">
+      <div id="5" className="card fluid">
         <div className="section">
           <h2>May I use pagination, sorting and filtering?</h2>
           <p>
-            Yes, you can add pagination, sorting and filtering options to your API
-            requests.
+            Yes, you can add pagination, sorting and filtering options to your
+            API requests.
           </p>
           <br />
           <h3>Pagination</h3>
@@ -252,10 +260,11 @@ function Documentation() {
             </tbody>
           </table>
           <h3>Filtering</h3>{" "}
-            <p>
-              The filtering works by casting simple url parameter expressions to mongodb lookup
-              expressions and can be applied to any available key on the data models.
-            </p>
+          <p>
+            The filtering works by casting simple url parameter expressions to
+            mongodb lookup expressions and can be applied to any available key
+            on the data models.
+          </p>
           <table className="table">
             <thead>
               <tr>
@@ -296,9 +305,11 @@ function Documentation() {
                 <td>
                   <strong>less than, greater than or equal to</strong>
                 </td>
-                <td>/movie?budgetInMillions{'<'}100</td>
-                <td>/movie?academyAwardWins{'>'}0</td>
-                <td>/movie?runtimeInMinutes{'>='}160</td>
+                <td>
+                  <p>/movie?budgetInMillions{"<"}100</p>
+                  <p>/movie?academyAwardWins{">"}0</p>
+                  <p>/movie?runtimeInMinutes{">="}160</p>
+                </td>
               </tr>
             </tbody>
           </table>
