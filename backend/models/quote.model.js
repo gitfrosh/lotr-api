3a757d0952b090d068fee7a8233cb77949784436
m.kikmunter <M.Kikmunter@gmail.com>
2021-07-12 10:43:55 +0100

    removed blanks on the quote model level

--- backend/models/quote.model.js
+++ backend/models/quote.model.js
@@ -1,25 +1,33 @@
 const mongoose = require("mongoose");
 var mongoosePaginate = require('mongoose-paginate');
 
+function removeBlanks(value) {
+  return value?.replace(/\s\s+/g, ' ')?.trim();
+};
+
 const quoteSchema = new mongoose.Schema(
   {
     dialog: {
-        type: String,
-        required: true
-      },
-      movie: {
-        type: mongoose.Types.ObjectId,
-        ref: "movie",
-        required: true
-      },
-      character: {
-        type: mongoose.Types.ObjectId,
-        ref: "character",
-        required: false
-      }
+      type: String,
+      required: true,
+      trim: true,
+      get: removeBlanks
+    },
+    movie: {
+      type: mongoose.Types.ObjectId,
+      ref: "movie",
+      required: true
+    },
+    character: {
+      type: mongoose.Types.ObjectId,
+      ref: "character",
+      required: false
+    }
   },
   {
     collection: "quotes",
+    toJSON: { getters: true },
+    toObject: { getters: true },
   },
   { timestamps: true }
 );
