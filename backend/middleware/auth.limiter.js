ee43f4b08f1b008bd9135c6f3fbad090d48f798f
m.kikmunter <M.Kikmunter@gmail.com>
2022-01-30 13:13:17 +0000

    made auth limiter to take options

--- backend/middleware/auth.limiter.js
+++ backend/middleware/auth.limiter.js
@@ -1,11 +1,6 @@
 const rateLimit = require('express-rate-limit');
 
-const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 create account requests per `window` (here, per hour)
-    message: 'Too many request from this IP, please try again after 15 mintues.',
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-
+const authLimiter = (options) => {
+    return rateLimit({ ...options })
+};
 module.exports = authLimiter;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,8 @@ var router = express.Router();
 const authLimiter = require("../middleware/auth.limiter");
 
 router.route("/login").post([passportHelpers.login, authController.login]);
-router.route("/register").post([authLimiter, authController.register]);
+//allow only 25 calls per 15 mins to this endpoint
+router.route("/register").post([authLimiter({ windowMs: 15 * 60 * 1000, max: 25 }), authController.register]);
 router.route("*").get(async function (req, res) {
   return res.status(404).send("Endpoint does not exist.");
 });
