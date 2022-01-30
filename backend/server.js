f2e56da0be581554b556f61569736c6ec82119e3
m.kikmunter <M.Kikmunter@gmail.com>
2022-01-30 13:39:36 +0000

    body-parser is no longer needed in express > 4.16

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ const mongoose = require("mongoose");
 const dpwcToken = process.env.DPWC_TOKEN || "";
 
 app.use(helmet());
-app.use(require("body-parser").json());
-app.use(require("body-parser").urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "/__BUILD"))); // React build
