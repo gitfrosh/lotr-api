7ff8cffa4fe59c890e415dd11e1dceeea8468c00
m.kikmunter <M.Kikmunter@gmail.com>
2022-01-30 20:54:01 +0000

    made limit more strict

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,8 +7,8 @@ var router = express.Router();
 const authLimiter = require("../middleware/auth.limiter");
 
 router.route("/login").post([passportHelpers.login, authController.login]);
-//allow only 25 calls per 15 mins to this endpoint
-router.route("/register").post([authLimiter({ windowMs: 15 * 60 * 1000, max: 25 }), authController.register]);
+//allow only 5 calls per hour to this endpoint
+router.route("/register").post([authLimiter({ windowMs: 60 * 60 * 1000, max: 5 }), authController.register]);
 router.route("*").get(async function (req, res) {
   return res.status(404).send("Endpoint does not exist.");
 });
