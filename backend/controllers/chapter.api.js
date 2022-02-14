b68d6493d7315113fb8838f6fec4ff24cc41fe39
m.kikmunter <M.Kikmunter@gmail.com>
2022-02-14 12:50:43 +0000

    changed chapter to use async await

--- backend/controllers/chapter.api.js
+++ backend/controllers/chapter.api.js
@@ -1,49 +1,42 @@
 const Chapter = require("./../models/chapter.model");
 const config = require("./../helpers/config");
 
+const errorResponse = {
+  success: false,
+  message: 'Something went wrong.',
+};
+
 module.exports = {
   getChapters: async (req, res) => {
     const options = await config.getOptions(req);
 
-    await Chapter.paginate(options.filter, {
-      ...options,
-      select: {
-        chapterName: 1,
-        book: 1
-      }
-    }, async function (
-      err,
-      chapter
-    ) {
-      if (err) {
-        return res.status(500).send({
-          success: false,
-          message: "Something went wrong.",
-        });
-      }
+    try {
+      const chapter = await Chapter.paginate(options.filter, {
+        ...options,
+        select: {
+          chapterName: 1,
+          book: 1
+        }
+      });
       return res.json(chapter);
-    });
+    } catch (err) {
+      return res.status(500).send(errorResponse);
+    }
   },
   getChapter: async (req, res) => {
     const options = await config.getOptions(req);
-    const id = req.params.id;
-    await Chapter.paginate({ _id: id }, {
-      ...options,
-      select: {
-        chapterName: 1,
-        book: 1
-      }
-    }, async function (
-      err,
-      chapter
-    ) {
-      if (err) {
-        return res.status(500).send({
-          success: false,
-          message: "Something went wrong.",
-        });
-      }
+    try {
+      const id = req.params.id;
+      const chapter = await Chapter.paginate({ _id: id }, {
+        ...options,
+        select: {
+          chapterName: 1,
+          book: 1
+        }
+      });
       return res.json(chapter);
-    });
+    } catch (err) {
+      return res.status(500).send(errorResponse);
+    }
   }
 }
