90f3bdc9c4a5e0bf24304c4eeab2db9f8c274660
Mateusz Kikmunter <mateusz.kikmunter@genesys.com>
2024-02-25 22:15:52 +0000

    almost working as the prev form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,42 +1,53 @@
-import React, { useState } from "react";
-import { login } from "../helpers/api";
+import React, { useState } from 'react';
+import { login } from '../helpers/api';
 import toast from 'react-hot-toast';
-import { useHistory } from "react-router-dom";
-import Helmet from "react-helmet";
+import { useHistory } from 'react-router-dom';
+import Helmet from 'react-helmet';
 
 async function setCookie(name, value, days) {
-  var expires = "";
+  var expires = '';
   if (days) {
     var date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
+    expires = '; expires=' + date.toUTCString();
   }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  document.cookie = name + '=' + (value || '') + expires + '; path=/';
 }
 
 function Login() {
   const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({ email: '', password: '' });
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isTouched, setIsTouched] = useState(false);
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  }
+  const handleInputChange = (field, value) => {
+    if (field === 'email') {
+      setEmail(value);
+    }
+    if (field === 'password') { 
+      setPassword(value);
+    }
+    setIsTouched(true);
+  };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  }
+  const validateField = (field) => {
+    if (!field.trim()) {
+      return 'Required'
+    };
+    return '';
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    if (!email || !password) {
-      setErrors({
-        email: email ? null : "Required",
-        password: password ? null : "Required"
-      });
+
+    const emailError = validateField(email);
+    const passwordError = validateField(password);
+
+    setErrors({ email: emailError, password: passwordError });
+
+    if (emailError || passwordError) {
       return;
     }
 
@@ -47,16 +58,16 @@ function Login() {
       if (response.message) {
         toast.error(response.message);
       } else {
-        toast.success("Login successful");
-        setCookie("lotr-api", response.token, 7);
+        toast.success('Login successful');
+        setCookie('lotr-api', response.token, 7);
         history.push('/account');
       }
     } catch (error) {
-      toast.error("An error occurred while logging in");
+      toast.error('An error occurred while logging in');
     } finally {
       setIsSubmitting(false);
     }
-  }
+  };
 
   return (
     <>
@@ -65,25 +76,34 @@ function Login() {
           <title>The Lord of the Rings API - The one API | Login</title>
         </Helmet>
         <form onSubmit={handleSubmit}>
-          <div className="input-group fluid">
+          <div className='input-group fluid'>
             <label>
-              E-Mail: <input type="email" value={email} onChange={handleEmailChange} />
-              {errors.email && <em>{errors.email}</em>}
+              E-Mail:{' '}
+              <input
+                type='email'
+                value={email}
+                onChange={(e) => handleInputChange('email', e.target.value)}
+                onBlur={() => setErrors({ ...errors, email: validateField(email) })}
+              />
+              {((isTouched && !email.trim()) || errors.email) && <em>Required</em>}
             </label>
           </div>
-          <div className="input-group fluid">
+          <div className='input-group fluid'>
             <label>
-              Password: <input type="password" value={password} onChange={handlePasswordChange} />
-              {errors.password && <em>{errors.password}</em>}
+              Password:{' '}
+              <input
+                type='password'
+                value={password}
+                onChange={(e) => handleInputChange('password', e.target.value)}
+                onBlur={() => setErrors({ ...errors, password: validateField(password) })}
+              />
+              {((isTouched && !password.trim()) || errors.password) && <em>Required</em>}
             </label>
           </div>
-          <div className="input-group fluid">
-            <button className="primary" type="submit" disabled={isSubmitting}>
-              Submit
+          <div className='input-group fluid'>
+            <button className='primary' type='submit' disabled={isSubmitting || !!errors.email || !!errors.password}>
+              {isSubmitting ? 'Submitting...' : 'Submit'}
             </button>
-          </div>
-          <div>
-            <em>{isSubmitting ? "Submitting..." : null}</em>
           </div>
         </form>
       </div>
