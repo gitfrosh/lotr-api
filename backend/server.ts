73e770edc4a240a27567ea02fe344f9fa1d5e865
Rike <hello@rike.dev>
2024-04-15 18:02:05 +0200

    fix: static files path

--- backend/server.ts
+++ backend/server.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '/__BUILD'))); // React build
+app.use(express.static(path.join(__dirname, '/../__BUILD'))); // React build
 
 const server_port = process.env.PORT || 3001;
 
