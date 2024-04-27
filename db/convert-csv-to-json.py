1345913ceb1ed6beb45656064113f8a2ebd74cea
Rike <hello@rike.dev>
2024-04-27 14:05:28 +0200

    fix: \

--- db/convert-csv-to-json.py
+++ db/convert-csv-to-json.py
@@ -4,7 +4,8 @@ import regex as re
 
 def transform_objectid(text):
     """Replace MongoDB ObjectId references to proper JSON format."""
-    return re.sub(r'ObjectId\((.*?)\)', r'{"$oid": "\1"}', text)
+    # Use non-capturing group and directly format the string with $oid.
+    return re.sub(r'ObjectId\(([^)]+)\)', r'{"$oid": "\1"}', text)
 
 def main():
     os.makedirs('db/json', exist_ok=True)  # Ensure the directory for JSON files exists
@@ -14,10 +15,10 @@ def main():
         df = pd.read_csv(f'db/csv/{file}')
         # Transform all string columns that may contain ObjectId references
         for column in df.select_dtypes(include=['object']):
-            df[column] = df[column].apply(lambda x: transform_objectid(str(x)) if x else x)
+            df[column] = df[column].apply(lambda x: transform_objectid(str(x)) if pd.notna(x) else x)
         # Save each dataframe as a JSON file with all objects in a single array
         json_path = f'db/json/{file.replace(".csv", ".json")}'
-        df.to_json(json_path, orient='records', indent=4, ensure_ascii=False)
+        df.to_json(json_path, orient='records', indent=4)
 
 if __name__ == "__main__":
     main()
